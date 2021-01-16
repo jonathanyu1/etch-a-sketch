e79e0138aad4cfbe56b0d7ec9504d1c6723cc93c
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-15 20:04:07 -0500

    created grid, added fill cells on hover

--- myscript.js
+++ myscript.js
@@ -1,13 +1,38 @@
-const gridContainer=document.querySelector('.grid-container');
+const gridContainer=document.querySelector('.gridContainer');
+let cellColor = 'black';
 
 function setGrid(size){
+    // empties cells
+    removeCells();
     for (i=0;i<size;i++){
         for (j=0;j<size;j++){
             const cell=document.createElement('div');
             cell.classList.add('cell');
             gridContainer.appendChild(cell);
+            cell.style.minWidth=(`40/${size}`);
+            cell.style.minHeight=(`40/${size}`);
+            // figure out how to trigger trail on click
+            // cell.addEventListener('click', () => {
+            cell.addEventListener('mouseover',()=>{
+                cell.style.backgroundColor= `${cellColor}`;
+            });
         }
+    }
+    setGridDimensions(size);
+}
+
+function setGridDimensions(size){
+    gridContainer.style.gridTemplateColumns = (`repeat(${size},1fr)`);
+    gridContainer.style.gridTemplateRows = (`repeat(${size},1fr)`);
+}
+
+function removeCells(){
+    while (gridContainer.hasChildNodes()){
+        gridContainer.removeChild(gridContainer.lastChild);
     }
 }
 
-setGrid(4);
+// starter grid
+setGrid(16);
+
+
